3aa96a3b3be027f8aec47c432fd47943125aa48b
Galek <galek1357@gmail.com>
2025-02-19 17:14:08 +0500

    Lab4 bug fixes

--- Lab4/src/app/app.component.spec.ts
+++ Lab4/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'product-list' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('product-list');
+    expect(app).toEqual('product-list');
   });
 
   it('should render title', () => {
